5d5e36811d1b81d3664434802c563f2a59c9646c
mnijafon <mniljf@sina.com>
2025-07-01 15:01:49 +0800

    init

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'bff-homework-202506',
+      script: './app.js',
+      instances: 'max', // 启动所有 CPU 核心
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+      },
+      error_file: './logs/bff-error.log',
+      out_file: './logs/bff-out.log',
+      merge_logs: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    },
+  ],
+};
